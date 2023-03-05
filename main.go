587dc57e2b2f6ae8e9f4f9b3b1f6b5b520a15d8a
Marco Ferretti <mferretti93@gmail.com>
2023-03-05 22:20:57 +0100

    fix audio download

--- main.go
+++ main.go
@@ -9,13 +9,17 @@ import (
 	"github.com/gocolly/colly"
 )
 
+var invalidFilenames = []string{
+	"jingle",
+}
+
 func main() {
 	crawl()
 }
 
 func crawl() {
 	_ = os.Mkdir("./download", os.ModePerm)
-	
+
 	mainCollector := colly.NewCollector(
 		colly.AllowedDomains("ageofempires.fandom.com", "www.ageofempires.fandom.com"),
 		colly.CacheDir("./cache"),
@@ -28,36 +32,6 @@ func crawl() {
 
 	mainCollector.OnRequest(func(r *colly.Request) {
 		log.Printf("Scraping URL: %s", r.URL.String())
-	})
-
-	soundsCollector.OnResponse(func(r *colly.Response) {
-		log.Printf("Downloading sound: %s", r.Request.URL.String())
-
-		contentType := r.Headers.Get("Content-Type")
-
-		if contentType != "application/ogg" {
-			return
-		}
-
-		contentDisposition := r.Headers.Get("Content-Disposition")
-		_, params, err := mime.ParseMediaType(contentDisposition)
-
-		if err != nil {
-			log.Printf("Error Content-Disposition header: %v", err)
-			return
-		}
-
-		filename := params["filename"]
-
-		path := "./download/" + params["filename"]
-		err = r.Save(path)
-		
-		if err != nil {
-			log.Printf("Error downloading sound: %v", err)
-			return
-		}
-
-		log.Printf("Downloaded sound %s into %s", filename, path)
 	})
 
 	mainCollector.OnHTML("h3 > span > a[href]", func(e *colly.HTMLElement) {
@@ -75,9 +49,47 @@ func crawl() {
 		mainCollector.Visit(absoluteLink)
 	})
 
-	mainCollector.OnHTML("audio", func(e *colly.HTMLElement) {
-		source := e.Attr("src")
+	mainCollector.OnHTML("span.audio-button", func(e *colly.HTMLElement) {
+		source := e.Attr("data-src")
 		soundsCollector.Visit(source)
+	})
+
+	soundsCollector.OnResponse(func(r *colly.Response) {
+		log.Printf("Downloading sound: %s", r.Request.URL.String())
+
+		contentType := r.Headers.Get("Content-Type")
+
+		if contentType != "application/ogg" {
+			log.Printf("Content-Type not valid: %s", contentType)
+			return
+		}
+
+		contentDisposition := r.Headers.Get("Content-Disposition")
+		_, params, err := mime.ParseMediaType(contentDisposition)
+
+		if err != nil {
+			log.Printf("Error parsing Content-Disposition header: %v", err)
+			return
+		}
+
+		filename := params["filename"]
+
+		for _, invalidFilename := range invalidFilenames {
+			if strings.Contains(filename, invalidFilename) {
+				log.Printf("Invalid filename, contains: %s", invalidFilename)
+				return
+			}
+		}
+
+		path := "./download/" + params["filename"]
+		err = r.Save(path)
+
+		if err != nil {
+			log.Printf("Error downloading sound: %v", err)
+			return
+		}
+
+		log.Printf("Downloaded sound %s into %s", filename, path)
 	})
 
 	mainCollector.Visit("https://ageofempires.fandom.com/wiki/Civilizations_(Age_of_Empires_II)")
